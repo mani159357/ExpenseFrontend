3f61980100997f287fafa35cf89ce07ad916eb2b
mani159357 <manikanthbolem2@gmail.com>
2023-12-27 16:51:22 +0530

    reset page updated

--- controller/resetpassword.js
+++ controller/resetpassword.js
@@ -67,19 +67,77 @@ const resetpassword = (req, res) => {
         if (forgotpasswordrequest) {
             forgotpasswordrequest.update({ active: false });
             res.status(200).send(`<html>
-                                    <script>
-                                        function formsubmitted(e){
-                                            e.preventDefault();
-                                            console.log('called')
-                                        }
-                                    </script>
 
-                                    <form action="/password/updatepassword/${id}" method="get">
-                                        <label for="newpassword">Enter New password</label>
-                                        <input name="newpassword" type="password" required></input>
-                                        <button>reset password</button>
-                                    </form>
-                                </html>`
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f4f4f4;
+                        margin: 0;
+                        padding: 0;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                    }
+            
+                    form {
+                        background-color: #ffffff;
+                        padding: 20px;
+                        border-radius: 8px;
+                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                        max-width: 400px;
+                        width: 100%;
+                    }
+            
+                    label {
+                        display: block;
+                        margin-bottom: 8px;
+                        font-weight: bold;
+                    }
+            
+                    input {
+                        width: 100%;
+                        padding: 8px;
+                        margin-bottom: 16px;
+                        box-sizing: border-box;
+                        border: 1px solid #ccc;
+                        border-radius: 4px;
+                    }
+            
+                    button {
+                        background-color: #4caf50;
+                        color: #fff;
+                        padding: 10px 15px;
+                        border: none;
+                        border-radius: 4px;
+                        cursor: pointer;
+                        font-size: 16px;
+                    }
+            
+                    button:hover {
+                        background-color: #45a049;
+                    }
+                </style>
+            </head>
+            
+            <body>
+                <form action="/password/updatepassword/${id}" method="get" onsubmit="formsubmitted(event)">
+                    <label for="newpassword">Enter New password</label>
+                    <input name="newpassword" type="password" required>
+                    <button>Reset Password</button>
+                </form>
+            
+                <script>
+                    function formsubmitted(e) {
+                        e.preventDefault();
+                        console.log('called');
+                    }
+                </script>
+            </body>
+            
+            </html>
+            `
             )
             res.end()
 
